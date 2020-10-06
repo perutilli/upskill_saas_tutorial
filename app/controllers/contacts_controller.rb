f3310f4a1546991a4bd27da3ae9f7f3f83cabe98
Alberto Perugini <albertoperugini.ap@gmail.com>
2020-10-06 16:09:08 +0000

    added comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,24 +1,31 @@
 class ContactsController < ApplicationController
+    
+    # GET request to /contact-us
+    # Show new contact form
     def new
         @contact = Contact.new
     end
     
+    # POST request /contacts
     def create
-       @contact = Contact.new(contact_params)
-       if @contact.save
-           name = params[:contact][:name]
-           email = params[:contact][:email]
-           body = params[:contact][:comments]
-           ContactMailer.contact_email(name, email, body).deliver
-           flash[:success] = "Message sent"
-           redirect_to new_contact_path
-       else
-           flash[:danger] = @contact.errors.full_messages.join(", ")
-           redirect_to new_contact_path
-       end
+        
+        @contact = Contact.new(contact_params)
+        if @contact.save
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            body = params[:contact][:comments]
+            ContactMailer.contact_email(name, email, body).deliver
+            flash[:success] = "Message sent"
+            redirect_to new_contact_path
+        else
+            # If contact object doesn't save
+            flash[:danger] = @contact.errors.full_messages.join(", ")
+            redirect_to new_contact_path
+        end
     end
     
     private
+        # To collect data from form we need to use strong parameters
         def contact_params
            params.require(:contact).permit(:name, :email, :comments)
         end
